310458258db3ccd2e7e6478198c9bbb5b3ac83d1
brugobi <brgobbi@hotmail.com>
2020-07-20 13:53:11 -0400

    add create link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,16 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.save
+    redirect_to article_path(@article)
+  end
   
 end
