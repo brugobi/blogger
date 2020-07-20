7cf204238cbe53333907ccb32b8a293696d1501f
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-20 14:50:11 -0500

    CREATE edit option for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,5 +23,16 @@ class ArticlesController < ApplicationController
     @article.destroy
     redirect_to articles_path
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  
+    redirect_to article_path(@article)
+  end
   
 end
