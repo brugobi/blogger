58b9c0aead9f44a1fc0a9fca0778bfb4ec813342
brugobi <brgobbi@hotmail.com>
2020-07-21 10:38:31 -0400

    fixed the destroy tag error

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
-  has_many :comments
-  has_many :taggings
-  has_many :tags, through: :taggings
+  has_many :comments, :dependent => :destroy
+  has_many :taggings, :dependent => :destroy
+  has_many :tags, through: :taggings, :dependent => :destroy
 
   def tag_list
     self.tags.collect do |tag|
